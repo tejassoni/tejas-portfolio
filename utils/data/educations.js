1e0e2532e325a95ef57d9980cb1dbdc87be17d6d
Tejas Soni <tejas.tejas.soni3@gmail.com>
2024-05-09 18:37:42 +0530

    initial commit

---
+++ utils/data/educations.js
@@ -0,0 +1,34 @@
+export const educations = [
+  {
+    id: 1,
+    title: "Master Degree",
+    course: "Master of Computer Application",
+    duration: "2011 - 2014",
+    college: "Shrimad Rajchandra Institute Of Management And Computer Application",
+    university: "Uka Tarsadia University"
+  },
+  {
+    id: 2,
+    title: "Bachelor Degree",
+    course: "Bachelor of Computer Application",
+    duration: "2009 - 2011",
+    college: "Shrimad Rajchandra Institute Of Management And Computer Application",
+    university: "Uka Tarsadia University"
+  },
+  {
+    id: 3,
+    title: "Higher Secondary Certificate",
+    course: "",
+    duration: "2007 - 2008",
+    college: "Jay Ambe Vidyalaya",
+    university: "Gujarat Secondary and Higher Secondary Education Board"
+  },
+  {
+    id: 4,
+    title: "Secondary School Certificate",
+    course: "",
+    duration: "2004 - 2005",
+    college: "Bright Day School",
+    university: "Gujarat Secondary and Higher Secondary Education Board"
+  }
+]
---
+++ utils/data/experience.js
@@ -0,0 +1,32 @@
+export const experiences = [
+  {
+    id: 1,
+    title: 'Senior Software Engineer',
+    company: "Bharti Soft Tech Pvt. Ltd",
+    duration: "(October 2023 - Present)"
+  },
+  {
+    id: 2,
+    title: "Laravel Team Leader",
+    company: "Sinelogix Web Technologies Pvt. Ltd",
+    duration: "(July 2019 - October 2023)"
+  },
+  {
+    id: 3,
+    title: "Senior PHP Developer",
+    company: "Viera Consulting Services LLP",
+    duration: "(August 2018 - July 2019)"
+  },
+  {
+    id: 4,
+    title: "Software Developer",
+    company: "Intransure Technologies Pvt. Ltd",
+    duration: "(May 2016 - April 2018)"
+  },
+  {
+    id: 5,
+    title: "Junior PHP Developer",
+    company: "Logic Express Pvt. Ltd",
+    duration: "(April 2015 - May 2016)"
+  }
+]
