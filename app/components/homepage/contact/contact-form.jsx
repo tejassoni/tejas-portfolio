d826c63cd5dfc728268c636b2c9cbcfa38a7aefb
Tejas Soni <tejas.tejas.soni3@gmail.com>
2024-05-18 16:51:24 +0530

    added contact form

--- app/components/homepage/contact/contact-form.jsx
+++ app/components/homepage/contact/contact-form.jsx
@@ -52,15 +52,15 @@ function ContactForm() {
       setError({ ...error, required: false });
     };
 
-    const serviceID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
-    const templateID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
-    const options = { publicKey: process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY };
+    const serviceID = 'service_8o6eij1';
+    const templateID = 'template_wv1hb35';
+    const options = { publicKey: 'qpDGEucAe_ZEMOnB0' };
 
     try {
       const res = await emailjs.send(serviceID, templateID, input, options);
 
       if (res.status === 200) {
-        toast.success('Message sent successfully!');
+        toast.success('Email Message sent successfully..!');
         setInput({
           name: '',
           email: '',
