8064f9817aa8a123c8184d4fe77ce34ee17d1351
Tejas Soni <tejas.tejas.soni3@gmail.com>
2024-05-19 17:12:19 +0530

    skills added

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -120,17 +120,25 @@ function HeroSection() {
                 <span className="text-gray-400">{"', '"}</span>
                 <span className="text-amber-300">CodeIgniter</span>
                 <span className="text-gray-400">{"', '"}</span>
+                <span className="text-amber-300">WordPress</span>
+                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-amber-300">Zend</span>
+                <span className="text-gray-400">{"', '"}</span>
                 <span className="text-amber-300">GoLang</span>
                 <span className="text-gray-400">{"', '"}</span>
                 <span className="text-amber-300">JavaScript</span>
                 <span className="text-gray-400">{"', '"}</span>
                 <span className="text-amber-300">jQuery</span>
+                <span className="text-gray-400">{"', '"}</span> 
+                <span className="text-amber-300">ReactJs</span>
                 <span className="text-gray-400">{"', '"}</span>
                 <span className="text-amber-300">MySql</span>
                 <span className="text-gray-400">{"', '"}</span>
                 <span className="text-amber-300">MongoDB</span>
                 <span className="text-gray-400">{"', '"}</span>
                 <span className="text-amber-300">Docker</span>
+                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-amber-300">Git</span>
                 <span className="text-gray-400">{"', '"}</span>
                 <span className="text-amber-300">AWS</span>
                 <span className="text-gray-400">{"', '"}</span>
@@ -189,7 +197,7 @@ function HeroSection() {
                 <span className="ml-12 lg:ml-24 text-cyan-400">this.</span>
                 <span className="mr-2 text-white">skills.length</span>
                 <span className="mr-2 text-amber-300">&gt;=</span>
-                <span className="text-orange-400">10</span>
+                <span className="text-orange-400">15</span>
               </div>
               <div><span className="ml-8 lg:ml-16 mr-2 text-gray-400">{`);`}</span></div>
               <div><span className="ml-4 lg:ml-8 text-gray-400">{`};`}</span></div>
