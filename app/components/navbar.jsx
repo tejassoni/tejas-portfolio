784f4d3ec1ebfeb0e5f6bbff81cb45d98a0a418a
Tejas Soni <tejas.tejas.soni3@gmail.com>
2024-05-19 16:53:47 +0530

    favicon added

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -33,7 +33,7 @@ function Navbar() {
             <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="https://tejassoni.github.io/tejas-portfolio/#projects"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">PROJECTS</div></Link>
           </li>
           <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="https://tejassoni.github.io/tejas-portfolio/#projects"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">Contact</div></Link>
+            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="https://tejassoni.github.io/tejas-portfolio/#contact"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">CONTACT</div></Link>
           </li>
         </ul>
       </div>
--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ import './css/globals.scss';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Portfolio of Tejas Soni - Expert Software Engineer | Lead',
+  title: 'Tejas Soni - Senior Software Engineer | Lead',
   description: 'Tejas Dilipkumar Soni, a seasoned Senior Software Engineer with over 9 years of extensive experience in the field. I am deeply passionate about programming and thrive on the challenges it presents. Throughout my career, I have honed my skills in various technologies, specializing in PHP frameworks like Laravel and CodeIgniter, as well as GoLang for high-performance applications With a strong foundation in database management, I haWWive worked extensively with MySQL and MongoDB, ensuring efficient data storage and retrieval. JavaScript is at the heart of my development journey, and I leverage its power along with libraries like jQuery and frameworks like React to create dynamic and user-friendly web applications. I am available for any kind of job opportunity that suits my skills and interests.',
 };
 
